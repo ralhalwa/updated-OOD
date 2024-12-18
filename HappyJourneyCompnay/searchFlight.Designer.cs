5c8db909059a4da76c49cb7c805705e6c105db14
ralhalwa <reemhalwachi42@gmail.com>
2024-12-18 22:25:30 +0300

    searchflight

--- HappyJourneyCompnay/searchFlight.Designer.cs
+++ HappyJourneyCompnay/searchFlight.Designer.cs
@@ -55,9 +55,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(36, 56);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 19);
+            this.label2.Size = new System.Drawing.Size(206, 29);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Enter flight ID:";
+            this.label2.Text = "Enter the country:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
--- HappyJourneyCompnay/searchFlight.cs
+++ HappyJourneyCompnay/searchFlight.cs
@@ -26,34 +26,76 @@ namespace HappyJourneyCompnay
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Get the country input from the user
+            string countryInput = textBox1.Text.Trim();
+
+            // Check if the user entered a valid country name
+            if (string.IsNullOrEmpty(countryInput))
+            {
+                MessageBox.Show("Please enter a valid destination country.");
+                return; // Exit if the input is empty
+            }
+
             using (SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database1.mdf;Integrated Security=True"))
             {
-                con.Open(); // Open connection
-
-                SqlCommand cmd = new SqlCommand("SELECT * FROM Flight WHERE flight_Id = @flight_id", con);
-                cmd.Parameters.AddWithValue("@flight_id", Convert.ToInt32(textBox1.Text));
-
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                richTextBox1.Clear();
-
-                if (reader.HasRows)
+                try
                 {
-                    while (reader.Read())
+                    con.Open(); // Open the connection
+
+                    // SQL query to search flights by departure or arrival destination country
+                    string query = @"
+                SELECT 
+                    f.flight_Id,
+                    f.departure_Time,
+                    f.arrival_Time,
+                    f.price,
+                    dep.destination_Country AS Departure_Country,
+                    arr.destination_Country AS Arrival_Country
+                FROM 
+                    Flight f
+                LEFT JOIN 
+                    Destination dep ON f.departure_Destination_Id = dep.destination_Id
+                LEFT JOIN 
+                    Destination arr ON f.arrival_Destination_Id = arr.destination_Id
+                WHERE 
+                    dep.destination_Country = @country OR arr.destination_Country = @country";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@country", countryInput);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    richTextBox1.Clear(); // Clear the previous results
+
+                    if (reader.HasRows)
                     {
-                        richTextBox1.AppendText("Flight ID: " + reader["flight_Id"].ToString() + "\n");
-                        richTextBox1.AppendText("Departure Time: " + reader["departure_Time"].ToString() + "\n");
-                        richTextBox1.AppendText("Arrival Time: " + reader["arrival_Time"].ToString() + "\n");
-                        richTextBox1.AppendText("Price: " + reader["price"].ToString() + "\n");
-                        richTextBox1.AppendText("Destination ID: " + reader["destination_Id"].ToString() + "\n\n");
+                        while (reader.Read())
+                        {
+                            richTextBox1.AppendText("Flight ID: " + reader["flight_Id"].ToString() + "\n");
+                            richTextBox1.AppendText("Departure Time: " + reader["departure_Time"].ToString() + "\n");
+                            richTextBox1.AppendText("Arrival Time: " + reader["arrival_Time"].ToString() + "\n");
+                            richTextBox1.AppendText("Price: " + reader["price"].ToString() + "\n");
+                            richTextBox1.AppendText("Departure Country: " + reader["Departure_Country"].ToString() + "\n");
+                            richTextBox1.AppendText("Arrival Country: " + reader["Arrival_Country"].ToString() + "\n\n");
+                        }
                     }
-                }
-                else
-                {
-                    richTextBox1.AppendText("No flight information avilable for this flight ID.");
-                }
+                    else
+                    {
+                        richTextBox1.AppendText("No flights available for the specified destination country.");
+                    }
 
-                reader.Close();
+                    reader.Close();
+                }
+                catch (SqlException sqlEx)
+                {
+                    // Handle SQL errors
+                    MessageBox.Show("Database Error: " + sqlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    // Handle general errors
+                    MessageBox.Show("Error: " + ex.Message);
+                }
             }
         }
 
